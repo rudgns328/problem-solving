aec8f9cf082497e203880081c421dd9bbe4bb0d7
kwonminsung0 <minsung21c@gmail.com>
2026-05-06 11:26:06 +0900

    feat: [민성] 배열 - 모의고사

---
+++ 01_Array/004_모의고사_[민성].py
@@ -0,0 +1,21 @@
+def solution(answers):
+    patterns = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+    scores = [0] * 3
+    
+    for i, answer in enumerate(answers):
+        for j, pattern in enumerate(patterns):
+            if answer == pattern[i % len(pattern)]:
+                scores[j] += 1
+                
+    max_score = max(scores)
+    high_score = []
+    
+    for i, score in enumerate(scores):
+        if max_score == score:
+            high_score.append(i + 1)
+    
+    return high_score
