7b31924c77ae620d67ab13cce1cbc1b10f37f271
rudgns328 <rudgns328@gmail.com>
2026-05-09 01:09:12 +0900

    feat: [경훈] 스택 - 10진수를 2진수로 변환하기

---
+++ 02_Stack/kyunghoon/009_10진수를_2진수로_변환하기.py
@@ -0,0 +1,12 @@
+def solution(decimal):
+    stack = []
+    while decimal > 0:
+        remainder = decimal % 2
+        stack.append(str(remainder))
+        decimal //= 2
+    binary = ""
+
+    while stack:
+        binary += stack.pop()
+
+    return binary
