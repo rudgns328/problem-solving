dc41511acb938c6b424a441c3db76be50cf8e48d
rudgns328 <rudgns328@gmail.com>
2026-05-19 23:26:58 +0900

    refactor: [경훈] 해시 - 할인 행사 코드 병목 해결하여 효율성 개선

--- 04_Hash/kyunghoon/021_할인_행사.py
+++ 04_Hash/kyunghoon/021_할인_행사.py
@@ -1,27 +1,29 @@
-from collections import deque
-from collections import Counter
+from collections import deque, Counter
 
 
 def solution(want, number, discount):
     w = dict(zip(want, number))
     discount = deque(discount)
     result = 0
+
     d = Counter(list(discount)[0:10])
 
     while len(discount) >= 10:
-        d = Counter(list(discount)[0:10])
-
         if w.keys() <= d.keys():
             for k in w:
                 if w[k] <= d[k]:
                     continue
                 else:
-                    discount.popleft()
                     break
             else:
                 result += 1
-                discount.popleft()
-        else:
-            discount.popleft()
+
+        out = discount.popleft()
+        d[out] -= 1
+        if d[out] == 0:
+            del d[out]
+
+        if len(discount) >= 10:
+            d[discount[9]] += 1
 
     return result
