8e8cdb691fab2e453e74e311ad7c3faf92febe8c
rudgns328 <rudgns328@gmail.com>
2026-05-20 17:58:00 +0900

    feat: [경훈] 해시 - 베스트 앨범

---
+++ 04_Hash/kyunghoon/023_베스트_앨범.py
@@ -0,0 +1,24 @@
+def solution(genres, plays):
+    answer = []
+    d = {}
+    for i, g in enumerate(genres):
+        if g not in d:
+            d[g] = {"total": 0}
+        d[g]["total"] += plays[i]
+        d[g][i] = plays[i]
+
+    sorted_genres = sorted(d, key=lambda x: d[x]["total"], reverse=True)
+
+    for g in sorted_genres:
+        sorted_songs = sorted(
+            {k: v for k, v in d[g].items() if k != "total"}.items(),
+            key=lambda x: x[1],
+            reverse=True,
+        )
+
+        for idx, (song_idx, _) in enumerate(sorted_songs):
+            if idx == 2:
+                break
+            answer.append(song_idx)
+
+    return answer
