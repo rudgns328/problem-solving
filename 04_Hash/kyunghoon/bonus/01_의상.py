90def3732e6cd8d44a4e34f9a7a8efb64266f643
rudgns328 <rudgns328@gmail.com>
2026-05-25 18:06:52 +0900

    refactor: [경훈] 해시 - 의상 - Counter 적용하여 코드 가독성 개선

--- 04_Hash/kyunghoon/bonus/01_의상.py
+++ 04_Hash/kyunghoon/bonus/01_의상.py
@@ -1,12 +1,9 @@
+from collections import Counter
+
+
 def solution(clothes):
     answer = 1
-    closet = {}
-
-    for c in clothes:
-        if c[1] in closet:
-            closet[c[1]] += 1
-        else:
-            closet[c[1]] = 1
+    closet = Counter(c[1] for c in clothes)
 
     for v in closet.values():
         answer *= v + 1
