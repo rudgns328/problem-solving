d36153819441cbf3d8a82aa87318d3be61b53fa1
rudgns328 <rudgns328@gmail.com>
2026-05-25 21:45:15 +0900

    refactor: [경훈] 해시 - 압축 - 누적 변수 활용하여 로직 개선

--- 04_Hash/kyunghoon/bonus/02_압축.py
+++ 04_Hash/kyunghoon/bonus/02_압축.py
@@ -1,21 +1,18 @@
 def solution(msg):
     answer = []
     alpha = {chr(i): i - 64 for i in range(65, 91)}
-    n = 26
-    i = 1
-    while msg:
-        if len(msg) == 1 or i > len(msg):
-            answer.append(alpha[msg])
-            msg = ""
-            break
-        elif msg[:i] in alpha:
-            i += 1
-            continue
+    n = 27
+    w = ""
+
+    for c in msg:
+        if w + c in alpha:
+            w += c
         else:
-            answer.append(alpha[msg[: i - 1]])
-            alpha[msg[:i]] = n + 1
+            answer.append(alpha[w])
+            alpha[w + c] = n
             n += 1
-            msg = msg[i - 1 :]
-            i = 1
+            w = c
+
+    answer.append(alpha[w])
 
     return answer
