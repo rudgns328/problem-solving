917e4c6bf4854fec8bf4b36863c7c1eaae106846
rudgns328 <rudgns328@gmail.com>
2026-06-04 22:29:51 +0900

    refactor: [경훈] 트리 - 양과 늑대 - 로직 개선

--- 05_Tree/kyunghoon/031_양과_늑대.py
+++ 05_Tree/kyunghoon/031_양과_늑대.py
@@ -1,24 +1,31 @@
-from collections import defaultdict, deque
+from collections import deque
 
 
 def solution(info, edges):
-    pc = defaultdict(list)
-    max_sheep = 1
-    for p, c in edges:
-        pc[p].append(c)
+    def build_tree(info, edges):
+        tree = [[] for _ in range(len(info))]
+        for edge in edges:
+            tree[edge[0]].append(edge[1])
+        return tree
 
-    queue = deque([(0, 1, 0, pc[0])])
-    while queue:
-        node, sheep, wolf, next_nodes = queue.popleft()
-        if wolf >= sheep:
-            continue
+    tree = build_tree(info, edges)
+    max_sheep = 0
 
-        next_nodes = next_nodes + pc[node]
-        for next_node in next_nodes:
-            new_next = [n for n in next_nodes if n != next_node]
-            new_sheep = sheep + (1 if info[next_node] == 0 else 0)
-            new_wolf = wolf + (1 if info[next_node] == 1 else 0)
-            max_sheep = max(max_sheep, new_sheep)
-            queue.append((next_node, new_sheep, new_wolf, new_next))
+    q = deque([(0, 1, 0, set())])
 
+    while q:
+        current, sheep_count, wolf_count, visited = q.popleft()
+        max_sheep = max(max_sheep, sheep_count)
+        visited.update(tree[current])
+
+        for next_node in visited:
+            if info[next_node]:
+                if sheep_count != wolf_count + 1:
+                    q.append(
+                        (next_node, sheep_count, wolf_count + 1, visited - {next_node})
+                    )
+                else:
+                    q.append(
+                        (next_node, sheep_count + 1, wolf_count, visited - {next_node})
+                    )
     return max_sheep
