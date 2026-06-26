3c3dfa67f94009ce70fa6f80672e1d6cf97096ce
rudgns328 <rudgns328@gmail.com>
2026-06-26 16:21:31 +0900

    refactor: [경훈] 그래프 - 합승 택시 요금 - dijkstra 메서드 선언으로 로직 개선

--- 07_Graph/kyunghoon/bonus/04_합승_택시_요금.py
+++ 07_Graph/kyunghoon/bonus/04_합승_택시_요금.py
@@ -1,73 +1,34 @@
 from heapq import heappush, heappop
-from collections import defaultdict
 
 INF = 10**9
 
 def solution(n, s, a, b, fares):
-    graph = defaultdict(list)
+    graph = [[] for _ in range(n + 1)]
     for u, v, w in fares:
         graph[u].append((v, w))
         graph[v].append((u, w))
-    s_dist = [INF] * (n + 1)
-    a_dist = [INF] * (n + 1)
-    b_dist = [INF] * (n + 1)
-    s_dist[s] = 0
-    a_dist[a] = 0
-    b_dist[b] = 0
-    s_visited = [False] * (n + 1)
-    a_visited = [False] * (n + 1)
-    b_visited = [False] * (n + 1)
-    s_priority_queue = [(0, s)]
-    a_priority_queue = [(0, a)]
-    b_priority_queue = [(0, b)]
     
-    while s_priority_queue:
-        current_dist, current_node = heappop(s_priority_queue)
+    def dijkstra(start):
+        dist = [INF] * (n + 1)
+        dist[start] = 0
+        heap = [(0, start)]
         
-        if s_visited[current_node]:
-            continue
-        
-        s_visited[current_node] = True
-        
-        for neighbor, weight in graph[current_node]:
-            new_distance = current_dist + weight
+        while heap:
+            cost, node = heappop(heap)
             
-            if s_dist[neighbor] > new_distance:
-                s_dist[neighbor] = new_distance
-                heappush(s_priority_queue, (new_distance, neighbor))
-                
-    while a_priority_queue:
-        current_dist, current_node = heappop(a_priority_queue)
-        
-        if a_visited[current_node]:
-            continue
-        
-        a_visited[current_node] = True
-        
-        for neighbor, weight in graph[current_node]:
-            new_distance = current_dist + weight
+            if dist[node] < cost:
+                continue
             
-            if a_dist[neighbor] > new_distance:
-                a_dist[neighbor] = new_distance
-                heappush(a_priority_queue, (new_distance, neighbor))
-                
-    while b_priority_queue:
-        current_dist, current_node = heappop(b_priority_queue)
+            for next_node, weight in graph[node]:
+                new_cost = cost + weight
+                if dist[next_node] > new_cost:
+                    dist[next_node] = new_cost
+                    heappush(heap, (new_cost, next_node))
         
-        if b_visited[current_node]:
-            continue
-        
-        b_visited[current_node] = True
-        
-        for neighbor, weight in graph[current_node]:
-            new_distance = current_dist + weight
-            
-            if b_dist[neighbor] > new_distance:
-                b_dist[neighbor] = new_distance
-                heappush(b_priority_queue, (new_distance, neighbor))
+        return dist
     
-    answer = min(s_dist[k] + a_dist[k] + b_dist[k] for k in range(1, n+1))
+    s_dist = dijkstra(s)
+    a_dist = dijkstra(a)
+    b_dist = dijkstra(b)
     
-    return answer
-
-print(solution(6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
+    return min(s_dist[k] + a_dist[k] + b_dist[k] for k in range(1, n + 1))
