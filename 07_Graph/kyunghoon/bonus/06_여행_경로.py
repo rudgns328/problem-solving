b57d2d064429886664147f8672c1e3d5510a926c
rudgns328 <rudgns328@gmail.com>
2026-06-28 19:52:51 +0900

    refactor: [경훈] 그래프 - 여행 경로 - 그래프 직접 수정 대신 visited 조회로 로직 개선

--- 07_Graph/kyunghoon/bonus/06_여행_경로.py
+++ 07_Graph/kyunghoon/bonus/06_여행_경로.py
@@ -1,26 +1,22 @@
-from collections import defaultdict
-
 def solution(tickets):
     answer = ["ICN"]
-    graph = defaultdict(list)
-    for k, v in tickets:
-        graph[k].append(v)
-    
-    for key in graph:
-        graph[key].sort()
-        
+    tickets.sort()
+    visited = [False] * len(tickets)
         
     def dfs(start):
         if len(answer) == len(tickets) + 1:
             return True
         
-        for i in range(len(graph[start])):
-            next_node = graph[start].pop(i)
-            answer.append(next_node)
-            if dfs(next_node):
-                return True
-            graph[start].insert(i, next_node)
-            answer.pop()
+        for i, (f, t) in enumerate(tickets):
+            if f == start and not visited[i]:
+                visited[i] = True
+                answer.append(t)
+
+                if dfs(t):
+                    return True
+
+                visited[i] = False
+                answer.pop()
         
     dfs("ICN")
     return answer
