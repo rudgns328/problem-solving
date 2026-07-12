6ca3f2c8ba757940a3771c3522f59dbab5ed4441
rudgns328 <rudgns328@gmail.com>
2026-07-12 18:03:05 +0900

    feat: [경훈] 정렬 - 정렬이 완료된 두 배열 합치기

---
+++ 09_Sort/kyunghoon/055_정렬이_완료된_두_배열_합치기.py
@@ -0,0 +1,16 @@
+def solution(arr1, arr2):
+    merged = []
+    i, j = 0, 0
+    
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] >= arr2[j]:
+            merged.append(arr2[j])
+            j += 1
+        else:
+            merged.append(arr1[i])
+            i += 1
+            
+    merged.extend(arr1[i:])
+    merged.extend(arr2[j:])
+        
+    return merged
