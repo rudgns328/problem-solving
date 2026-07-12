e4f95cbc907a9408ed4bbc85c59f56b2c3b37c21
rudgns328 <rudgns328@gmail.com>
2026-07-12 16:45:08 +0900

    feat: [경훈] 정렬 - 계수 정렬 구현하기

---
+++ 09_Sort/kyunghoon/054_계수_정렬_구현하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    count = [0] * 26
+    
+    for c in s:
+        count[ord(c) - ord("a")] += 1
+        
+    sorted_str = ""
+    for i in range(26):
+        sorted_str += chr(i + ord("a")) * count[i]
+
+    return sorted_str
+
+print(solution("hello"))
