ec6f151f7326e82ee41652b771d7713c604b36aa
rudgns328 <rudgns328@gmail.com>
2026-07-31 13:45:32 +0900

    refactor: [경훈] 정렬 - 파일명 정렬 - re 라이브러리 사용하여 로직 개선

--- 09_Sort/kyunghoon/bonus/02_파일명_정렬.py
+++ 09_Sort/kyunghoon/bonus/02_파일명_정렬.py
@@ -1,16 +1,7 @@
-def solution(files):
-    
-    def make_key(filename):
-        i = 0
-        while i < len(filename) and not filename[i].isdigit():
-            i += 1
-        head = filename[:i]
+import re
 
-        j = i
-        while j < len(filename) and filename[j].isdigit():
-            j += 1
-        number = filename[i:j]
-        
-        return (head.lower(), int(number))
-        
-    return sorted(files, key=make_key)
+def solution(files):
+    a = sorted(files, key=lambda file : int(re.findall('\d+', file)[0]))
+    b = sorted(a, key=lambda file : re.split('\d+', file.lower())[0])
+
+    return b
