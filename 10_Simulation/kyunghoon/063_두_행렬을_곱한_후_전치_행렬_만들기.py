763380fd9dacff23c935ea0fb6fa9176f224cbc1
rudgns328 <rudgns328@gmail.com>
2026-08-11 15:52:55 +0900

    refactor: [경훈] 시뮬레이션 - 두 행렬을 곱한 후 전치 행렬 만들기 - 불필요한 변수 제거

--- 10_Simulation/kyunghoon/063_두_행렬을_곱한_후_전치_행렬_만들기.py
+++ 10_Simulation/kyunghoon/063_두_행렬을_곱한_후_전치_행렬_만들기.py
@@ -2,9 +2,9 @@ def solution(matrix1, matrix2):
     res = [[0] * 3 for _ in range(3)]
     for i in range(3):
         for j in range(3):
-            total = 0
             for k in range(3):
-                total += matrix1[j][k] * matrix2[k][i]
-            res[i][j] = total
+                res[i][j] += matrix1[j][k] * matrix2[k][i]
             
     return res
+
+print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[9, 8, 7], [6, 5, 4], [3, 2, 1]]))
