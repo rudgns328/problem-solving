6d58af52da274711d8f94b711fdd863261083b7a
rudgns328 <rudgns328@gmail.com>
2026-08-11 15:53:21 +0900

    refactor: [경훈] 시뮬레이션 - 두 행렬을 곱한 후 전치 행렬 만들기 - 출력문 제거

--- 10_Simulation/kyunghoon/063_두_행렬을_곱한_후_전치_행렬_만들기.py
+++ 10_Simulation/kyunghoon/063_두_행렬을_곱한_후_전치_행렬_만들기.py
@@ -6,5 +6,3 @@ def solution(matrix1, matrix2):
                 res[i][j] += matrix1[j][k] * matrix2[k][i]
             
     return res
-
-print(solution([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[9, 8, 7], [6, 5, 4], [3, 2, 1]]))
