dd311a0f51391d2393d69141bcd5756d9d5950b6
rudgns328 <rudgns328@gmail.com>
2026-08-17 15:22:37 +0900

    refactor: [경훈] 시뮬레이션 - 점프와 순간 이동 - bin() 메서드로 로직 개선

--- 10_Simulation/kyunghoon/068_점프와_순간_이동.py
+++ 10_Simulation/kyunghoon/068_점프와_순간_이동.py
@@ -1,10 +1,2 @@
 def solution(n):
-    ans = 0
-    while n != 0:
-        if n % 2 == 0:
-            n //= 2
-        else:
-            n -= 1
-            ans += 1
-            
-    return ans
+    return bin(n).count('1')
