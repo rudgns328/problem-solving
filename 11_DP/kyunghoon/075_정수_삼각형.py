e1a7b0deea66596eaa5d79f6d57b3083ea33c53e
rudgns328 <rudgns328@gmail.com>
2026-08-24 16:33:09 +0900

    refactor: [경훈] 동적계획법 - 정수 삼각형 - 상향식 접근으로 로직 개선

--- 11_DP/kyunghoon/075_정수_삼각형.py
+++ 11_DP/kyunghoon/075_정수_삼각형.py
@@ -1,15 +1,12 @@
 def solution(triangle):
     n = len(triangle)
-    dp = [[0] * (i + 1) for i in range(n)]
-    dp[0][0] = triangle[0][0]
+    dp = [[0] * n for _ in range(n)]
 
-    for i in range(1, n):
+    for i in range(n):
+        dp[n-1][i] = triangle[n-1][i]
+        
+    for i in range(n-2, -1, -1):
         for j in range(i + 1):
-            if j == 0:
-                dp[i][j] = dp[i-1][j] + triangle[i][j]
-            elif j == i:
-                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
-            else:
-                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
-                
-    return max(dp[n-1])
+            dp[i][j] = max(dp[i+1][j], dp[i+1][j+1]) + triangle[i][j]
+
+    return dp[0][0]
