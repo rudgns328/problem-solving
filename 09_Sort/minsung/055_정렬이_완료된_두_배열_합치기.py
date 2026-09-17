2e4ee33b65955092a041442dba5c8f4a38e3a234
kwonminsung0 <minsung21c@gmail.com>
2026-09-17 23:30:03 +0900

    feat: [민성] 정렬 - 정렬이 완료된 두 배열 합치기

---
+++ 09_Sort/minsung/055_정렬이_완료된_두_배열_합치기.py
@@ -0,0 +1,20 @@
+def solution(arr1, arr2):
+    merged = []
+    i, j = 0, 0
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] <= arr2[j]:
+            merged.append(arr1[i])
+            i += 1
+        else:
+            merged.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):
+        merged.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        merged.append(arr2[j])
+        j += 1
+
+    return merged
