6fd7d87477ed9cf1a0d5b810ccf86511bed1fc3a
kwonminsung0 <minsung21c@gmail.com>
2026-09-21 17:01:28 +0900

    feat: [민성] 정렬 - 가장 큰 수

---
+++ 09_Sort/minsung/059_가장_큰_수.py
@@ -0,0 +1,21 @@
+from functools import cmp_to_key
+
+
+def solution(numbers):
+    nums = list(map(str, numbers))
+
+    def compare(a, b):
+        if a + b > b + a:
+            return -1
+        elif a + b < b + a:
+            return 1
+        else:
+            return 0
+
+    nums.sort(key=cmp_to_key(compare))
+    answer = "".join(nums)
+
+    if answer == 0:
+        return "0"
+
+    return answer
