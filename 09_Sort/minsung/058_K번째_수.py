fce3e70907dc3c7ac665247391d47f395d329cd3
kwonminsung0 <minsung21c@gmail.com>
2026-09-21 16:28:45 +0900

    feat: [민성] 정렬 - K번째 수

---
+++ 09_Sort/minsung/058_K번째_수.py
@@ -0,0 +1,8 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i, j, k = command
+        nums = array[i - 1 : j]
+        nums.sort()
+        answer.append(nums[k - 1])
+    return answer
